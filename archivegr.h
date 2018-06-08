0a8fcf5cd5f2af6d044e6fd5cc18d2bb4a7564d6
korybilak <39914545+korybilak@users.noreply.github.com>
2018-06-08 09:55:40 -0400

    Create archivegr.h

---
+++ archivegr.h
@@ -0,0 +1,18 @@
+#ifndef ARCHIVEMGR_H
+#define ARCHIVEMGR_H
+
+#include <stdio.h>
+
+#define ARCHIVE "monthlydata.log"
+
+/* Structures */
+
+
+/* Functions */
+int openarchive();
+int retrieveaverages();
+
+
+/* Variables */
+
+#endif
